da024310f9ecd214e937338f312249b79e501675
Chansik Yun <chansigi@gmail.com>
2018-06-14 11:50:19 +0900

    .

--- src/unittest/ex1/CalculatorTest.java
+++ src/unittest/ex1/CalculatorTest.java
@@ -15,13 +15,44 @@ package unittest.ex1;
 
 import org.junit.Test;
 
+// import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
 // junit4 / junit5 => AOP(관점 지향 프로그래밍)
 // => alt + enter
 public class CalculatorTest {
+    /*
     @Test
     public void testAdd() {
         // System.out.println("CalculatorTest - testAdd()");
         throw new IllegalStateException("TODO");
+    }
+    */
+
+    // 테스트를 구성하는 방법 - 3A
+    // 1) Arrange: 객체를 생성하고, 필요한 경우 적절하게 설정한다.
+    // 2) Act: 객체에 작용을 가한다.
+    // 3) Assert: 기대하는 바를 단언한다.
+
+    // 테스트를 만들때 고려할 점
+    //  * 테스트 코드 안에는 조건문이나 반복문 같은 제어구문이 나오면 안됩니다.
+
+    @Test
+    public void addTest() {
+        // Arrange
+        Calculator calc = new Calculator();
+
+        // Act
+        calc.add(10);
+        calc.add(32);
+
+        // Assert
+        assertEquals(42, calc.display());
+        /*
+        if (42 != calc.display()) {
+            throw new IllegalStateException("10 + 32 != 42");
+        }
+        */
     }
 }
 
@@ -50,7 +81,6 @@ public class Program {
 // SUT: System Under Test
 //  => CUT(Code Under Test, Class Under Test)
 //  => 테스트 대상 시스템
-
 class Calculator {
     private int value;
 
