d7c3119fbc4dc0fae26bb139671fbdb5b7f7bf1a
Chansik Yun <chansigi@gmail.com>
2018-06-15 15:00:17 +0900

    .

--- src/testdouble/ex8/MockitoExamples.java
+++ src/testdouble/ex8/MockitoExamples.java
@@ -5,15 +5,59 @@ package testdouble.ex8;
 // 3. 메소드 호출 순서
 // 4. 메소드 인자(그룹)
 
+import org.junit.Test;
+
 import java.util.List;
+
+import static org.mockito.Mockito.*;
 
 class SUT {
     public void foo(List<String> s) {
         s.add("one");
         s.add("two");
     }
+
+    public void goo(List<String> s) {
+        s.add("once");
+        s.add("twice");
+        s.add("twice");
+    }
 }
 
 public class MockitoExamples {
+    // 1. 호출 여부 판단
+    @Test
+    public void fooTest() {
+        SUT sut = new SUT();
+        List<String> mockedList = mock(List.class);
+
+        sut.foo(mockedList);
+
+        verify(mockedList).add("one");
+        verify(mockedList).add("two");
+    }
+
+    // 2. 호출 횟수 판단
+
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
